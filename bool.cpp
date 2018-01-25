eca1eb70d6d1bf7a86bdc46c0c20f0fa52f02b59
RoyJH <royjh@okstate.edu>
2018-01-24 20:25:19 -0600

    and/or
    more flow control

--- bool.cpp
+++ bool.cpp
@@ -4,22 +4,40 @@ using namespace std;
 int main()
 {
 	int room_length = 5, room_width = 10;
-	float room_radius = 10.0;
-	float room_circle_area = (room_radius * room_radius) * 3.14;
-	
-	cout << "Enter your Length: ";
-	cin >> room_length;
+	//float room_radius = 10.0;
+	//float room_circle_area = (room_radius * room_radius) * 3.14;
+	bool keep_running = true;
+	char question = 'y';
+	while (keep_running) {
 
-	cout << "Enter your Width: ";
-	cin >> room_width;
+		/* ==equal
+		 != not equal
+		 > greater than
+		 < less than
+		 <= less than or equal too
+		 >= greater than or equal too
+		 || left OR right is true
+		 && left AND right are true
+		*/
+		cout << "Enter your Length: ";
+		cin >> room_length;
 
-	if (room_length == room_width) {
-		cout << "Length and Width are the same." << endl;
-	} else {
-		cout << "Length and Width are not the same." << endl;
-		cout << "Lenght is: " << room_length << endl;
-		cout << "Width is: " << room_width << endl;
+		cout << "Enter your Width: ";
+		cin >> room_width;
+
+		if (room_length == room_width) {
+			cout << "Length and Width are the same." << endl;
+		}
+		else {
+			cout << "Length and Width are not the same." << endl;
+			cout << "Lenght is: " << room_length << endl;
+			cout << "Width is: " << room_width << endl;
+
+			cout << "Run again (Y/y): ";
+			cin >> question;
+			if ((question != 'y') && (question != 'Y')){ keep_running = false; }
+		}
 	}
-
 	return 0;
 }
+
